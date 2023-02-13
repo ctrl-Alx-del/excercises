4590d16010a5606bb8c0631a3c68964d519c2bf0
Alex <electromanati@gmail.com>
2023-02-13 15:11:04 +0100

    capitalization lavet om til en funktion

--- capitalization.js
+++ capitalization.js
@@ -1,5 +1,12 @@
-let theName = "peter";
+let theName = "petER";
 
-theName = theName[0].toUpperCase() + theName.substring(1, theName.length);
+// theName = theName[0].toUpperCase() + theName.substring(1).toLowerCase();
 
-console.log(theName);
+// console.log(theName);
+
+function capitalization(str) {
+  str = str[0].toUpperCase() + str.substring(1).toLowerCase();
+  console.log(str);
+}
+
+capitalization(theName);
