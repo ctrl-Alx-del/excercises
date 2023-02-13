a426be5827766063fa5aa868b0217febbbf22966
Alex <electromanati@gmail.com>
2023-02-13 15:41:49 +0100

    nameparts FullName excercise

--- nameparts.js
+++ nameparts.js
@@ -15,22 +15,44 @@ const changeName = 2;
 // console.log({ firstName, middleName, lastName });
 
 //-------------------------------Med funktion i stedet for---------------------------------
-function getNameParts(fullname) {
-  const nameSplitted1 = fullname.indexOf(" ");
-  const nameSplitted2 = fullname.indexOf(" ", nameSplitted1 + 1);
-  const nameSplitted3 = fullname.lastIndexOf(" ");
+// function getNameParts(fullname) {
+//   const nameSplitted1 = fullname.indexOf(" ");
+//   const nameSplitted2 = fullname.indexOf(" ", nameSplitted1 + 1);
+//   const nameSplitted3 = fullname.lastIndexOf(" ");
 
-  const firstName = fullname.substring(0, nameSplitted1);
-  let middleName = fullname.substring(nameSplitted1, nameSplitted3);
-  const lastName = fullname.substring(nameSplitted3);
+//   const firstName = fullname.substring(0, nameSplitted1);
+//   let middleName = fullname.substring(nameSplitted1, nameSplitted3);
+//   const lastName = fullname.substring(nameSplitted3);
 
+//   //sets middlename to undefined if nothing is found
+//   if (middleName === "") {
+//     middleName = undefined;
+//   }
+
+//   // console.log(firstName + "\n", middleName + "\n", lastName + "\n");
+//   console.log({ firstName, middleName, lastName });
+//   //Der kommer ekstra spaces når man kalder funktionen fullName indeni getNameParts!
+//   //fullName(lastName, firstName, middleName);
+// }
+
+// getNameParts("Monkey Junkey Skunky");
+
+//---------------------------FullName excercise--------------------------------------------
+
+function fullName(lastName, firstName, middleName) {
   //sets middlename to undefined if nothing is found
-  if (middleName === "") {
-    middleName = undefined;
+  if (middleName === undefined) {
+    middleName = "";
   }
 
-  // console.log(firstName + "\n", middleName + "\n", lastName + "\n");
-  console.log({ firstName, middleName, lastName });
+  if (middleName) {
+    middleName = middleName + " ";
+  }
+
+  console.log(firstName + " " + middleName + lastName);
 }
 
-getNameParts("Monkey Junkey");
+fullName("Potter", "Harry", "James");
+fullName("Potter", "Harry");
+fullName("Harry", "Potter");
+fullName("Potter", "Harry", "James", "Pottypotpot");
