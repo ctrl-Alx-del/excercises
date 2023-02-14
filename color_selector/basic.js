d3684a4742996d17dfa5ce583dbe96af3d4e1dab
Alex <electromanati@gmail.com>
2023-02-14 12:53:39 +0100

    color selector set up

---
+++ color_selector/basic.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function cssToRGB(rgb) {
+  let r = rgb.substring(rgb.indexOf("(") + 1, rgb.indexOf(","));
+  let g = rgb.substring(rbg.indexOf(","), rbg.lastIndexOf(","));
+  let b;
+  console.log(r);
+}
+
+cssToRGB("rgb(192, 13, 1)");
---
+++ return_values/basic.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//---------------------------------Excercise 1--------------------------------------------
+
+// function greeting(honeyBadger) {
+//   return `Hello ${honeyBadger}`;
+// }
+
+// const result = greeting("Peter");
+
+// console.log("Result: ", result);
+
+// //this returns the value of the function to the console log without
+// //storing it in a variable first
+// console.log("Result: ", greeting("Timmy"));
+
+//---------------------------------Excercise 2--------------------------------------------
+
+function greeting(honeyBadger) {
+  return `Hello ${honeyBadger}`;
+}
+
+// greeting("any name");
+
+// console.log(greeting("any name"));
+
+// resultatet er: Hello undefined
+// console.log(greeting());
+
+// Man kan have funktionen inde i en template literal og stadig få det til at virke
+// const txt = `Greetings is ${greeting("Alexander")}`;
+// console.log(txt);
