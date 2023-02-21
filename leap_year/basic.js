0e4e438a3640f5030b2d27e8c26b05b468235a05
Alex <electromanati@gmail.com>
2023-02-21 17:20:39 +0100

    leap year excercise

---
+++ leap_year/basic.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function isLeapYear(year) {
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 400 === 0 && year % 100 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
